b88c8509f408709ef08bd830c47e209466db466a
Tomas Vasquez <tomamais@gmail.com>
2020-11-01 17:56:35 -0500

    Iniciando a aula 4
    - Aplicada a logica de permitir apenas uma inimigo por vez na tela.

---
+++ aula_4/src/scripts/jogo/personagem.js
@@ -0,0 +1,57 @@
+class Personagem extends Animacao {
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+    this.variacaoY = variacaoY;
+    this.yInicial = height - altura - this.variacaoY;
+
+    this.y = this.yInicial;
+    this.velocidadeDoPulo = 0;
+    this.gravidade = 6;
+    this.alturaDoPulo = -50;
+    this.pulos = 0;
+  }
+
+  pula() {
+    // controla a quantidade de pulos sequenciais
+    if (this.pulos < 2) { this.velocidadeDoPulo = this.alturaDoPulo; this.pulos++; }
+  }
+
+  aplicaGravidade() {
+    this.y = this.y + this.velocidadeDoPulo;
+    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade;
+
+    if (this.y > this.yInicial) {
+      this.y = this.yInicial;
+      // reseta a quantidade de pulos
+      this.pulos = 0;
+    }
+  }
+
+  estaColidindo(inimigo) {
+    const precisao = .7;
+
+    // debug
+    // noFill();
+    // rect(this.x + (this.largura - (this.largura * precisao))/2,
+    //   this.y + (this.altura - (this.altura * precisao))/2,
+    //   this.largura * precisao,
+    //   this.altura * precisao);
+    // rect(inimigo.x + (inimigo.largura - (inimigo.largura * precisao))/2,
+    //   inimigo.y + (inimigo.altura - (inimigo.altura * precisao))/2,
+    //   inimigo.largura * precisao,
+    //   inimigo.altura * precisao); 
+    // end debug
+
+    const colisao = collideRectRect(
+      this.x + (this.largura - (this.largura * precisao))/2,
+      this.y + (this.altura - (this.altura * precisao))/2,
+      this.largura * precisao,
+      this.altura * precisao,
+      inimigo.x + (inimigo.largura - (inimigo.largura * precisao))/2,
+      inimigo.y + (inimigo.altura - (inimigo.altura * precisao))/2,
+      inimigo.largura * precisao,
+      inimigo.altura * precisao);
+
+    return colisao;
+  }
+}
